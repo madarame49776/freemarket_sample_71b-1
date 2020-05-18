f1bf8d6d00b258dcf23d5d5698d4209971bbc551
ito-soichro <queen.fm1235@gamil.com>
2020-05-18 22:40:40 +0900

    hamlの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  $date = Time.now.in_time_zone('Tokyo').to_s
+  root "products#index"
+  resources :products, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

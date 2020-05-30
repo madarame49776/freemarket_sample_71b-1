31b7dbe9fc87976030c4612cf121b7048631298e
ito-soichro <queen.fm1235@gamil.com>
2020-05-30 23:58:31 +0900

    deviseの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+    keys: [:nickname,  :birthday, :first_name,
+           :last_name, :first_name_kana, :last_name_kana,])
+  end
+
 
   #Basic認証の導入
   def basic_auth

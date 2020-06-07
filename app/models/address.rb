92de5cedaccee5baad475ba557bc9b2fa3817fe8
hiroto-hash <hiroto5118@icloud.com>
2020-06-07 17:05:09 +0900

    商品出品ページ住所

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,8 @@
 class Address < ApplicationRecord
   #確認
   belongs_to :user, optional: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 
   with_options presence: true do
     validates :post_number, format: {with: /\A\d{3}[-]\d{4}\z/}

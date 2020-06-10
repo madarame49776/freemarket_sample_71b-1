29b401bb80c3b9c78880b29c6909e81a1be17106
ito-soichro <queen.fm1235@gamil.com>
2020-06-10 14:40:02 +0900

    ファイル整理のため

---
+++ db/migrate/20200610043559_create_products.rb
@@ -0,0 +1,20 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string     :name,        null: false
+      t.text       :description, null: false
+      t.integer    :price,       null: false
+      t.integer    :condition,   null: false, default: 0
+      t.integer    :status,      null: false, default: 0
+      t.string     :brand
+      t.integer    :send_price,  null: false, default: 0
+      t.integer    :ship_day,    null: false, default: 0
+      t.bigint     :user_id,    null: false
+      t.integer    :category_id, null: false
+      t.integer    :prefecture_id, null: false
+      t.timestamps
+    end
+  end
+end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_124228) do
+ActiveRecord::Schema.define(version: 2020_06_10_043559) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_number", null: false
@@ -22,6 +22,22 @@ ActiveRecord::Schema.define(version: 2020_05_30_124228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "condition", default: 0, null: false
+    t.integer "status", default: 0, null: false
+    t.string "brand"
+    t.integer "send_price", default: 0, null: false
+    t.integer "ship_day", default: 0, null: false
+    t.bigint "user_id", null: false
+    t.integer "category_id", null: false
+    t.integer "prefecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

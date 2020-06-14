27a0c0c235ecf3cbf55b38fecc212457dab128b5
ito-soichro <queen.fm1235@gamil.com>
2020-06-14 11:48:51 +0900

    DB修正のため

---
+++ db/migrate/20200614024219_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.string        :post_number,   null:false
+      t.string        :city,          null:false
+      t.string        :block,         null:false
+      t.string        :address,       null:false
+      t.string        :bulding_name,  null:false
+      t.references    :user,          foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

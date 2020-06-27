ef3a0944847c1b18d4ea75603e436e2fa48cec3a
ito-soichro <queen.fm1235@gamil.com>
2020-06-27 21:25:18 +0900

    カテゴリー蘭の表示完成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @newProducts = Product.includes(:images).where(status: 0).order("RAND()")
     @parents = Category.where(ancestry: nil)
   end
 end

d6c02e656e98ffac6ef1576fef557fcf0814367f
ito-soichro <queen.fm1235@gamil.com>
2020-06-29 15:41:02 +0900

    カテゴリー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,17 @@ class ProductsController < ApplicationController
   before_action :set_product, except: [:index, :new, :create]
 
   def index
-    @parent = Category.where(ancestry: nil)
+    # @parent = Category.where(ancestry: nil)
   end
 
   def new 
     @product = Product.new
     @product.images.new
+    @parent = ["選択してください"]
+    #データベースから、親カテゴリーのみ抽出し、配列化
+      Category.where(ancestry: nil).each do |parent|
+      @parent << parent.name
+      end
   end 
 
   def create
@@ -37,6 +42,22 @@ class ProductsController < ApplicationController
   def destroy
   end
 
+ # 親カテゴリーが選択された後に動くアクション
+  def children_category
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_categories = Category.where(ancestry: params[:parent_category_id])
+    render json: @category_categories
+  end
+
+# 子カテゴリーが選択された後に動くアクション
+  def grandchildren_category
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @grandchildren_categories = Category.where(ancestry: "#{params[:parent_category_id]}/#{params[:children_category_id]}")
+    render json: @grandchildren_categories
+  end
+
+
+
   private
 
   def product_params
@@ -46,8 +67,10 @@ class ProductsController < ApplicationController
 
   def set_product
     @product = Product.find(patams[:id])
-
   end
-
+  
+  def set_categories
+    @categories = Category.where(ancestry: nil)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,14 @@ Rails.application.routes.draw do
   
   root "home#index"
   
-  resources :products
+  resources :products, only: [:index, :show, :new, :edit, :destroy] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
+  
   resources :users, only: [:index, :show, :new, :edit]
 
   resources :card, only: [:new, :show] do

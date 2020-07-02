c2711195aee54a97cefd70dd647fbb9a7e3b56c5
Fukuda57 <joichiro.57.f@icloud.com>
2020-07-02 12:42:39 +0900

    add-show-function#1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,17 @@
 class Product < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   belongs_to :category, optional: true
   has_many :images
   belongs_to :user
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  enum condition:   { brand_new: 0, near_to_unused: 1, no_scrach: 2, slight_scrach: 3, has_scrach: 4, bad_state: 5 }
+  enum send_price:  { postage_included: 0, cash_on_delivery: 1 }
+  enum ship_day:    { early_day: 0, mid_day: 1, late_day: 2 }
+
   
   with_options presence: true do
     validates :name
